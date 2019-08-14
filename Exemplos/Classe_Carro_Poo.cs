1810693ee560c996e48de5e0c0b80fb763599c4f
VINICIUS CARDOSO MARANO <vinicius.cmarano@senacsp.edu.br>
2019-08-14 10:54:26 -0300

    Adicionando Arquivo Classe_Carro

---
+++ Exemplos/Classe_Carro_Poo.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classe
+{
+    class Carro
+    {
+        // PROPRIEDADES OU CARACTERÍSTICAS
+
+        public string Marca = "Fiat";
+        public int AnoModelo;
+        public bool LuzesInternas = false;
+
+
+
+        // MÉTODOS OU AÇÕES
+        public void abrirPortas()
+
+        {
+            // IMAGINE QUE O ALGUÉM IRÁ ABRIR A PORTA
+            // DO CARRO E AS LUZES VÃO ACENDER.  
+            LuzesInternas = true;
+        }
+
+        public void fecharPortas()
+
+        {
+            // IMAGINE QUE O PASSAGEIRO FECHOU A PORTA
+            // DO CARRO E AS LUZES VÃO APAGAR.
+            LuzesInternas = false;
+        }
+
+        /* ESSE MÉTODO IRÁ RETORNAR UMA INFORMAÇÃO PARA O USUÁRIO
+         * NESTE EXEMPLO, VAMOS IMAGINAR QUE ELE IRÁ LIGAR OS FAROIS
+         * MAS EU PRECISO SABER QUAL A INTENSIDADE. */
+
+        public string acenderFarolAutomaticamente()
+
+        {
+            return "Médio";
+
+        }
+
+
+
+        }
+}
